b2e1eb8a89bb77574b52dfd5ce384dfecbc2854c
conqryash007 <guptayash200010@gmail.com>
2021-11-23 15:40:54 +0530

    Input form

--- src/places/pages/Newplace.js
+++ src/places/pages/Newplace.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../../shared/components/Navbar/Navbar";
+import Input from "./../../shared/components/FormElements/Input";
 
 export default function Users() {
   return (
     <>
       <Navbar />
-      <h1>Places List</h1>
+      <h1 style={{ textAlign: "center", color: "white" }}>Add New Place</h1>
+      <Input></Input>
     </>
   );
 }
